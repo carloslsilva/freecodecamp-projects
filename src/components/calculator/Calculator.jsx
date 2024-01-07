a3ab1db4ea667c5c2727c9ad6f6c8fe0f2cb8e49
Carlos Silva <clsilva78@gmail.com>
2024-01-07 17:59:14 -0500

    Changed border radius on calculator container

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -8,7 +8,7 @@ export const Calculator = () => {
   useCalculatorKeyPress()
 
   return (
-    <div className='rounded-md border-[1px] border-alpha px-4 pb-4 pt-9'>
+    <div className='rounded-md border-2 border-alpha px-4 pb-4 pt-9'>
       <Display operator={operator} value={display} />
       <ButtonPad onClick={handleClick} />
     </div>
