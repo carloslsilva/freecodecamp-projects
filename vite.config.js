034ab7605058ead62d0e3c2e9afb38823c803987
Carlos Silva <clsilva78@gmail.com>
2024-01-07 23:38:42 -0500

    Remove alias

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve('./src')
-    }
-  },
   plugins: [react()]
 })
