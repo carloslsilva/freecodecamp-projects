a4c516a8c2c0ec97f3ff85a933ee7376c6ad2d62
Carlos Silva <clsilva78@gmail.com>
2024-01-09 04:28:02 -0500

    Remove padding in display component

--- src/apps/calculator/components/Display.jsx
+++ src/apps/calculator/components/Display.jsx
@@ -1,5 +1,5 @@
 export const Display = ({ operator, value }) => (
-  <div className='px-1 text-right font-display'>
+  <div className='text-right font-display'>
     <div className='h-5 text-[1rem] opacity-60'>{operator}</div>
     <div className='pb-5 pt-2 text-[2rem]' id='display'>
       {value}
